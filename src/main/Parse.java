931eb6472612a3b4b56f34c0eecab22d74f26e58
johanndrouillard <johanndrouillard@gmail.com>
2019-11-05 12:19:27 +0100

    rectif

--- src/main/Parse.java
+++ src/main/Parse.java
@@ -8,10 +8,21 @@ public class Parse {
 
     private final static String JSON_WEATHER_PATH = "weather.json";
 
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+    public class Parse {
+
+        private final static String JSON_WEATHER_PATH = "weather.json";
+
     public static void main(String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            JsonNode root = ObjectMapper.readTree(new File("weather.json"));
+            JsonNode root = objectMapper.readTree(new File(JSON_WEATHER_PATH));
 
             String cityName = root.get("name").asText();
 
